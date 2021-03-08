768de86c3003ae5f4797003f8dad66fe62c1af9f
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2021-03-08 20:00:51 +0200

    couple collection lessons

--- Collections/FlatMap/src/Task.kt
+++ Collections/FlatMap/src/Task.kt
@@ -1,7 +1,7 @@
 // Return all products the given customer has ordered
 fun Customer.getOrderedProducts(): List<Product> =
-        TODO()
+        this.orders.flatMap { it.products }
 
 // Return all products that were ordered by at least one customer
 fun Shop.getOrderedProducts(): Set<Product> =
-        TODO()
+        this.customers.flatMap { it.getOrderedProducts() }.toSet()
