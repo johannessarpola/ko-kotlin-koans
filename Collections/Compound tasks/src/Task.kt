041446f85c87f1a1596c3ff60366f4ecaa95d044
Johannes Sarpola <johannes.sarpola@bilot.fi>
2021-03-09 13:11:20 +0200

    Compound tasks

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,14 +1,17 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
+    return customer.getOrderedProductsDelivered().maxBy { it.price }!!
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return this.customers.flatMap { it.getOrderedProductsDelivered() }.count { it == product}
 }
 
-fun Customer.getOrderedProducts(): List<Product> =
-        /* TODO */
+fun Customer.getOrderedProducts(): List<Product> = this.orders.flatMap { it.products }
+
+fun Customer.getOrderedProductsDelivered(): List<Product> = this.orders
+    .filter(Order::isDelivered)
+    .flatMap(Order::products)
