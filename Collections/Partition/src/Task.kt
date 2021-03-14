5646a5062b1acfe8d02a27a48541de283246e7b1
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2021-03-14 16:14:43 +0200

    Rest of the collection koans

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,8 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> {
+    return customers.partition { customer ->
+        val del: Int = customer.orders.count { it.isDelivered }
+        val undel: Int = customer.orders.size - del
+        undel > del
+    }.first.toSet()
+}
